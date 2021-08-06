c101911ea56fcd181bd1027e7f9f210e260bf735
Gabby <fgabrielle1445@yahoo.com>
2021-08-06 13:40:39 -0600

    Added files for future webpages and Components.

--- comic-suggestion-site/src/Files/allComics.js
+++ comic-suggestion-site/src/Files/allComics.js
@@ -22,4 +22,23 @@ Template for comic suggestions
 },
 */
 
-export default []
+export default [
+    {
+        'title': 'American Vampire',
+        'publisher': 'DC',
+        'issue': 1,
+        'volume': 1,
+        'publication-date': '',
+        'writer': ['Scott Snyder', 'Stephen King'],
+        'artist': ['Rafael Albuquerque', 'Mateus Santolouco'],
+        'genre': ['Gothic Western', 'Weird Western', 'Western', 'Horror', 'Action', 'Vampires', 'Literature', 'Mature'],
+        'characters': ['Pearl Jones', 'Skinner Sweet'],
+        'description': 'Pearl Jones is a struggling young actress in 1920s Los Angeles. But when her big break brings her face-to-face with an ancient evil, her Hollywood dream quickly turns into a brutal, shocking nightmare. Plus, in a back-up tale by horror master Stephen King, learn the origins of the first American Vampire!',
+        'additionalTags': [''],
+        'additionalInfo': 'I will put a suggestion here!',
+        'suggestionBy': 'Gabby',
+        'fileName': '',
+        'pubUrl': '',
+        'comixologyUrl': 'https://www.comixology.com/American-Vampire-1/digital-comic/11821?ref=c2VyaWVzL3ZpZXcvZGVza3RvcC9ncmlkTGlzdC9jb21peG9sb2d5VW5saW1pdGVk',
+    },
+]
