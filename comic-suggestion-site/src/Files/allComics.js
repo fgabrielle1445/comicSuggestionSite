82854d69aeb7420a1942181584bf8e781207a2d1
Gabby <fgabrielle1445@yahoo.com>
2021-08-06 13:27:16 -0600

    Added a hardcoded js file for storing suggested comics.

---
+++ comic-suggestion-site/src/Files/allComics.js
@@ -0,0 +1,25 @@
+// Need to add a POST feature to make this easier
+
+/*
+Template for comic suggestions
+{
+	'title': '',
+	'publisher': '',
+	'issue': #,
+	'volume': #,
+	'publication-date': '',
+	'writer': [''],
+	'artist': [''],
+	'genre': [''],
+	'characters': [''],
+	'description': '',
+	'additionalTags': [''],
+	'additionalInfo': '',
+	'suggestionBy': '',
+	'fileName': '',
+	'pubUrl': '',
+	'comixologyUrl': '',
+},
+*/
+
+export default []
